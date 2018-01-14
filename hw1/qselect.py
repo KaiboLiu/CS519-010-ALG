9dfc0683eedd9d9c269027804d87fd6f291f0b2a
KaiboLiu <liukaibo87@gmail.com>
2018-01-14 03:50:01 -0800

    updated

--- hw1/qselect.py
+++ hw1/qselect.py
@@ -1,14 +1,15 @@
 from random import randint
 
 def qselect_sort(k,a):
-    if len(a)<k:return a
-    pivot = a[randint(0,len(a)-1)]
-    a[0],pivot = pivot, a[0]
+    if len(a) < k: return a
+    pidx = randint(0,len(a)-1)
+    a[0], a[pidx] = a[pidx], a[0]
+    pivot = a[0]
     left = [x for x in a[1:] if x<pivot] + [pivot]
     llen = len(left)
-    if llen==k:
+    if llen == k:
         return left
-    if llen>k:
+    if llen > k:
         return qselect_sort(k,left)
     else:
         right = [x for x in a[1:] if x>=pivot]
@@ -18,8 +19,14 @@ def qselect(k,a):
     if k > len(a): k = len(a)
     if k < 1: k = 1
     b = qselect_sort(k,a)
-    return b
-  
+    return b[-1]
+'''
+
+a = [11,8,5,1,5,2,7,5]
+print(sorted(a))
+print(a)  
 for i in range(0,10):  
     #print('k =',i, qselect(i,[11,8,4,1,5,2,7,9]))
+
     print('k =',i, qselect(i,[11,8,5,1,5,2,7,5]))
+'''
