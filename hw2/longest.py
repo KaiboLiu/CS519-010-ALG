19e03d750a8bcdd07d41be1c111a8f7b3ec47569
KaiboLiu <liukaibo87@gmail.com>
2018-01-16 12:38:39 -0800

    updated

---
+++ hw2/longest.py
@@ -0,0 +1,29 @@
+def Lpath(tree):
+	if tree == []: return -1,0
+	if tree[0] == [] and tree[2] == []: return 0,0
+
+	l_dep, l_path = Lpath(tree[0])
+	r_dep, r_path = Lpath(tree[2])
+	this_path  = l_dep + 1 if l_dep >= 0 else 0
+	this_path += r_dep + 1 if r_dep >= 0 else 0
+	
+	return max(l_dep,r_dep)+1, max(this_path,l_path,r_path)  #return no_this_node, pass_this_node
+	### return a,b is equivalent to return (a,b), it's a tuple, not list
+
+def longest(tree):
+	dep, path = Lpath(tree)
+	return max(dep, path)
+
+
+if __name__ == "__main__":
+	print('longest([[], 1, []]):')
+	print(longest([[], 1, []]))
+
+	print('longest([[[], 1, []], 2, [[], 3, []]]):')
+	print(longest([[[], 1, []], 2, [[], 3, []]]))
+
+	print('longest([[[[], 1, []], 2, [[], 3, []]], 4, [[[], 5, []], 6, [[], 7, [[], 9, []]]]]):')
+	print(longest([[[[], 1, []], 2, [[], 3, []]], 4, [[[], 5, []], 6, [[], 7, [[], 9, []]]]]))
+
+	print('longest([[], 2, [[[[], 6, []], 4, []], 3, [[], 5, [[], 7, []]]]]):')
+	print(longest([[], 2, [[[[], 6, []], 4, []], 3, [[], 5, [[], 7, []]]]]))
--- server_push.sh
+++ server_push.sh
@@ -1,3 +1,3 @@
 #! /bin/zsh
 
-scp -r /Users/zhangyilin/0_pipi/LKB_OSU/2018W-CS519-010_Alg/homeworks/* liukaib@flop.engr.oregonstate.edu:/nfs/stak/users/liukaib/cs519-010
+scp -r /Users/zhangyilin/0_pipi/LKB_OSU/2018W-CS519-010_Alg/homeworks/hw* liukaib@flop.engr.oregonstate.edu:/nfs/stak/users/liukaib/cs519-010
