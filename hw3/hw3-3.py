a5ae4058c4d195424eb470388409a1ac4b693dbe
Kaibo Liu <liukaibo87@gmail.com>
2018-01-23 22:48:47 -0800

    compatible with negative entries

--- hw3/hw3-3.py
+++ hw3/hw3-3.py
@@ -7,17 +7,21 @@ Created on Tue Jan 23 00:05:58 2018
 def find(a):
     res = []
     a.sort()
-    for k in range (2, len(a)):
-        i, j, mid = 0, k-1, a[k]/2
-        while i < j and a[i]<mid and a[j]>mid:
-            while i < j and a[i]+a[j] < a[k]: i = i+1
-            while i < j and a[i]+a[j] > a[k]: j = j-1
-            if i < j and a[i]+a[j] == a[k]:
-                res.append((a[i],a[j],a[k]))
+    for z in a:
+        i, j= 0, len(a)-1
+        while i < j:# and a[i]<mid and a[j]>mid:
+            while i < j and a[i]+a[j] < z: i = i+1
+            while i < j and a[i]+a[j] > z: j = j-1
+            if a[i] == z: i = i + 1
+            if a[j] == z: j = j - 1
+            if i < j and a[i]+a[j] == z:
+                res.append((a[i],a[j],z))
                 i = i + 1
                 j = j - 1
     return res
     
 if __name__ == "__main__":
     a = [1,4,2,3,5]
-    print(find(a))
+    print(find([1,4,2,3,5]))
+    print(find([1,-4,0,-3,-1]))
+    print(find([1,-4,0,-3,-1,-2,3,4,5]))
