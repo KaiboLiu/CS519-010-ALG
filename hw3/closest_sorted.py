5a167924fa03aafb6c75b8e26d995d96ab8d9515
Kaibo Liu <liukaibo87@gmail.com>
2018-01-25 00:03:55 -0800

    Update closest_sorted.py

--- hw3/closest_sorted.py
+++ hw3/closest_sorted.py
@@ -11,14 +11,19 @@ def find(a, x, k):
         return a
     j = bisect(a, x)    # equals bisect_right()
     i, count, l = j - 1, 0, len(a)
-    diff_l, diff_r = abs(a[i]-x), abs(a[j]-x)
+    #diff_l, diff_r = abs(a[i]-x), abs(a[j]-x)
     while (count < k):
-        if (j == l) or (i >= 0 and diff_l <= diff_r):
+        if (j == l):
+            return a[-k:]
+        if (i == 0):
+            return a[:k]
+        #if (diff_l <= diff_r):
+        if (abs(a[i]-x) <= abs(a[j]-x)):
             i = i - 1
-            diff_l = abs(a[i]-x)
+            #diff_l = abs(a[i]-x)
         else:
             j = j + 1
-            diff_r = abs(a[j]-x)
+            #diff_r = abs(a[j]-x)
         count = count + 1
     
     return a[i+1:j]
