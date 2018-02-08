13faadfc149aa1aa5e3186a3a0c53147aa453d04
Kaibo Liu <liukaibo87@gmail.com>
2018-02-07 20:40:57 -0800

    update

--- hw4/datastream.py
+++ hw4/datastream.py
@@ -13,10 +13,9 @@ def ksmallest(k,a):
     # O(k+(n-k)logk*2+klogk)=O(2nlogk-klogk+k)
 '''
 
-def ksmallest(k,a):
+def ksmallest0(k,a):
     h = []
     i = 0
-
     for x in a:
         if i >= k: 
             if -x <= h[0]: continue
@@ -27,10 +26,27 @@ def ksmallest(k,a):
             
     return [-i for i in heapq.nlargest(k,h)]
 
+def ksmallest(k,a):
+    i, h = 0, []
+    it = iter(a)
+    while True:
+        try:
+            x = next(it)
+            if i >= k:
+                if -x > h[0]:  heapq.heapreplace(h,-x)
+            else:
+                heapq.heappush(h,-x)
+                i += 1
+        except StopIteration:
+            break
+
+    return [-i for i in heapq.nlargest(k,h)]
+        
+
 
 if __name__ == "__main__":
-    print(ksmallest(4, [10, 2, 9, 3, 7, 8, 11, 5, 7]))
+    print(ksmallest0(4, [10, 2, 9, 3, 7, 8, 11, 5, 7]))
     #[2, 3, 5, 7]
-    print(ksmallest(3, range(1000000, 0, -1)))
+    print(ksmallest0(3, range(1000000, 0, -1)))
     #[1, 2, 3]
     
