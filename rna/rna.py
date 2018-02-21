3d3321d7eb0495ddaafc386c7a7f975d54c4c5c0
Kaibo Liu <liukaibo87@gmail.com>
2018-02-20 17:59:38 -0800

    update

--- rna/rna.py
+++ rna/rna.py
@@ -1,11 +1,46 @@
+from collections import defaultdict
+
+p = {'AU','UA','CG','GC','UG','GU'}
+
 def best(s):
-    l = len(s)
+    opt=defaultdict(int)
+    pair, l = {}, len(s)
+    strc = ['.' for _ in range(l)]
+    if l <= 1: return 0
+    for d in range(1,l):  # r=delta range
+        for i in range(l-d):    # j=i+d
+            j = i+d
+            for k in range(i+1,i+d-1):
+                opt[i,j] = max(opt[i,k]+opt[k+1,j],opt[i,j])
+            opt[i,j] = max(opt[i,j],max(opt[i+1,j], opt[i,j-1]))
+            if opt[i+1,j-1] >= opt[i,j]:
+                opt[i,j] = opt[i+1,j-1]
+                if s[i]+s[j] in p:
+                    opt[i,j] += 1
+                    pair[i] = j
+            
+
+    def solution(a,b):
+        if a >= b: return 
+        for i in range(a,b):
+            if i in pair and pair[i] <= b:
+                strc[i], strc[pair[i]] = '(', ')' 
+                solution(a,i-1)
+                solution(i+1,pair[i]-1)
+                solution(pair[i]+1,b)
+                return
+
+    solution(0,l-1)
+    
+    return opt[0,l-1], ''.join(strc)
+
+
 
 
 if __name__ == "__main__":
     
     s = "ACAGU"
-    print(s)
+    print(best(s))  #(2, '((.))')
     s = "UUCAGGA"
     print(best(s))  #(3, '(((.)))')
     s = "GUUAGAGUCU"
@@ -42,7 +77,7 @@ if __name__ == "__main__":
     print(best(s))  #(18, '(()())(((((.)))()(((())(.(.().()()))))))')
     s = "AACCGCUGUGUCAAGCCCAUCCUGCCUUGUU"
     print(best(s))  #(11, '(((.(..(.((.)((...().))()))))))')
-
+    '''
     import time
     from random import randint,seed
     seed(10)   #random.seed
@@ -61,4 +96,4 @@ if __name__ == "__main__":
         
         print('len=%d len_res=%s \nbottom-up: %0.5fs\ntop-down: %0.5fs'%(l,len(res),time1-time0, time2-time1))
    
-
+    '''
