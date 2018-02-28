9be28d4fd7e101a866c99e62e11c6f70a1a3ec50
Kaibo Liu <liukaibo87@gmail.com>
2018-02-28 10:38:55 -0800

    update

--- rna/rna.py
+++ rna/rna.py
@@ -4,20 +4,23 @@ import pdb
 p = {'AU','UA','CG','GC','UG','GU'}
 
 def best(s):
-    opt = defaultdict(int)
-    pair, l = {}, len(s)
-    strc = ['.' for _ in range(l)]
+    opt, strc = defaultdict(int), defaultdict(lambda:'.')
+    l = len(s)
+
     if l <= 1: return 0
     for d in range(2,l+1):  # d=delta range, length of range[i,j]:s[i]..s[j-1]
         for i in range(l-d+1):    # j=i+d
             j = i+d            
-            for k in range(i+1,j):  #k = i+1..j-1
-                opt[i,j] = max(opt[i,k]+opt[k,j],opt[i,j])
-            if opt[i+1,j-1] >= opt[i,j] and s[i]+s[j-1] in p:
+            if s[i]+s[j-1] in p:
                 opt[i,j] = opt[i+1,j-1] + 1
-                pair[i] = j-1
-                print(i,j-1, opt[i,j], s[i], s[j-1])
-            
+                strc[i,j] = '(' + strc[i+1,j-1] + ')'
+            for k in range(i+1,j):  #k = i+1..j-1
+                if opt[i,k]+opt[k,j] > opt[i,j]:
+                    opt[i,j] = opt[i,k]+opt[k,j]
+                    strc[i,j] = strc[i,k] + strc[k,j]
+            if opt[i,j] == 0:
+                strc[i,j] = '.' * (j-i)
+    '''        
     def solution(a,b,n):
         if a+2 > b: return 
         for i in range(a,b-1):
@@ -29,22 +32,19 @@ def best(s):
                 solution(i+1,pair[i],opt[i,pair[i]+1]-1)
                 solution(pair[i]+1,b,opt[pair[i]+1,b])
                 return
-
+    
     solution(0,l,opt[0,l])
-    pdb.set_trace()
+    '''
+    #pdb.set_trace()
     #print(pair)
     
-    return opt[0,l], ''.join(strc)
+    return opt[0,l], strc[0,l]
 
 
 
 
 if __name__ == "__main__":
-    #s = "GAUGCCGUGUAGUCCAAAGACUUCACCGUUGG"
-    #print(best(s))  #(14, '.()()(()(()())(((.((.)(.))()))))')
-    s = "GUGUAGUCCAAAGACUUC"
-    print(best(s))  #(14, '.()()(()(()())(((.((.)(.))()))))')
-    '''
+
     s = "ACAGU"
     print(best(s))  #(2, '((.))')
     s = "UUCAGGA"
@@ -83,7 +83,7 @@ if __name__ == "__main__":
     print(best(s))  #(18, '(()())(((((.)))()(((())(.(.().()()))))))')
     s = "AACCGCUGUGUCAAGCCCAUCCUGCCUUGUU"
     print(best(s))  #(11, '(((.(..(.((.)((...().))()))))))')
-    
+    '''
     import time
     from random import randint,seed
     seed(10)   #random.seed
