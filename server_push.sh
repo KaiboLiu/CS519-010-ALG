71149ed6e483c60b373db3ed84c6e683f6223443
Kaibo Liu <liukaibo87@gmail.com>
2018-03-06 15:09:13 -0800

    update

--- server_push.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/zsh
-
-scp -r /Users/kaibo/LKB@OSU/2018W-CS519-010-Algorithms/homeworks/hw* liukaib@flop.engr.oregonstate.edu:/nfs/stak/users/liukaib/algor
-scp -r /Users/kaibo/LKB@OSU/2018W-CS519-010-Algorithms/homeworks/rna liukaib@flop.engr.oregonstate.edu:/nfs/stak/users/liukaib/algor
