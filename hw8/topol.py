85e82e78e5cf96ef9310eba9e306462c62b05d1c
Kaibo Liu <liukaibo87@gmail.com>
2018-03-21 11:05:52 -0700

    update

--- hw8/topol.py
+++ hw8/topol.py
@@ -52,9 +52,41 @@ def order(n, edges):
     if i < len(degree): return None            
     return res
 
+
+## added on 03/20/2018
+## DFS
+def order_DFS1(n, edges):
+    def DFS(v,res): 
+        color[v] = -1
+        print('v:',v)
+        for u in pred[v]:
+            if color[u] < 0: continue
+            degree[u] -= 1
+            if degree[u] == 0:
+                DFS(u,res)
+                res.append(u)
+        
+        #print(res)
+
+    degree, pred = defaultdict(int), defaultdict(list)
+    color = defaultdict(int)    # color: -1:black/popped, 0: white/not_visited, 1: grey/visited
+    res = []   # i is the open pointer in queue 'nodes'
+
+    for (u,v) in edges:
+        degree[v] = degree[v]
+        degree[u] += 1
+        pred[v].append(u)
+    for u in range(n):      # add a sink
+        degree[u] += 1
+        pred[n].append(u)
+    #print(degree)
+    DFS(n,res)
+    print(res)
+    return res if len(res) == n else None
+
 ## added on 03/16/2018
 ## DFS
-def order2(n, edges):
+def order_DFS2(n, edges):
     def DFS(v,res): 
         if color[v] == 1: return False  # visit a grey one twice
         color[v] = 1
@@ -82,16 +114,47 @@ def order2(n, edges):
     acyclic = DFS(n,res)
     return res[:-1] if acyclic else None
 
+
+## added on 03/20/2018
+## DFS
+def order_DFS3(n, edges):
+    def DFS(v,res): 
+        if color[v] == 1: return False  # visit a grey one twice
+        color[v] = 1
+        for u in pred[v]:
+            if color[u] >= 0:
+                if not DFS(u,res): return False
+        if degree[v] == 0:
+            res.append(v)
+            color[v] = -1
+            for v1 in pred[v]:
+                if color[v1] >= 0: degree[v1] -= 1
+        return True
+
+    for i in range(n):
+        edges.append((i,n))
+
+    degree, pred = defaultdict(int), defaultdict(list)
+    color = defaultdict(int)    # color: -1:black/popped, 0: white/not_visited, 1: grey/visited
+    res = []   # i is the open pointer in queue 'nodes'
+    for (u,v) in edges:
+        degree[v] = degree[v]
+        degree[u] += 1
+        pred[v].append(u)
+    acyclic = DFS(n,res)
+    return res[:-1] if acyclic else None
+
+
 if __name__ == "__main__":
-    print(order2(8, [(0,2), (1,2), (2,3), (2,4), (3,4), (3,5), (4,5), (5,6), (5,7)]))
+    print(order_DFS3(8, [(0,2), (1,2), (2,3), (2,4), (3,4), (3,5), (4,5), (5,6), (5,7)]))
     #[0, 1, 2, 3, 4, 5, 6, 7]
-    print(order2(8, [(0,2), (1,2), (2,3), (2,4), (3,4), (3,5), (4,5), (5,6),(5,6),(5,6), (5,7)]))
+    print(order_DFS3(8, [(0,2), (1,2), (2,3), (2,4), (3,4), (3,5), (4,5), (5,6),(5,6),(5,6), (5,7)]))
     #[0, 1, 2, 3, 4, 5, 6, 7]
-    print(order2(11, [(0,2), (1,2), (2,3), (2,4), (3,4), (3,5), (4,5), (5,6),(5,6),(5,6), (5,7),(9,10)]))
+    print(order_DFS3(11, [(0,2), (1,2), (2,3), (2,4), (3,4), (3,5), (4,5), (5,6),(5,6),(5,6), (5,7),(9,10)]))
     #[0, 1, 9, 2, 10, 3, 4, 5, 6, 7]
-    print(order2(8, [(0,2), (1,2), (2,3), (2,4), (4,3), (3,5), (4,5), (5,6), (5,7)]))
+    print(order_DFS3(8, [(0,2), (1,2), (2,3), (2,4), (4,3), (3,5), (4,5), (5,6), (5,7)]))
     #[0, 1, 2, 4, 3, 5, 6, 7]
-    print(order2(4, [(0,1), (1,2), (2,1), (2,3)]))
+    print(order_DFS3(4, [(0,1), (1,2), (2,1), (2,3)]))
     #None
 
 
